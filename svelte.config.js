2b9cbd88151d2c236cabb230138d2f686417866f
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-11 20:27:25 +0200

    .

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		paths: {
+			base: process.argv.includes('dev') ? '/dev' : process.env.BASE_PATH
+		},
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
