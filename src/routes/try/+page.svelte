6c411184a425c4a444b2c4e7f7c6daf33f5746b5
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-11 21:33:14 +0200

    .

--- src/routes/try/+page.svelte
+++ src/routes/try/+page.svelte
@@ -1,5 +1,22 @@
 <script>
     import {base} from "$app/paths";
+    const d0 = new Date()
+    var d1 = d0.toISOString().split('T')[0], d2 = d1
+    d0.setDate(d0.getDate() + 1)
+    var d2 = d0.toISOString().split('T')[0]
+    $: sub = (new Date(d2) - new Date(d1)) / (24 * 60 * 60 * 1000)
 </script>
 
 <a href="{base}/">Amőba</a>
+<hr>
+<h3>Dátum input</h3>
+<input type="date" bind:value={d1}>
+<br><br>
+{d1.split("-").join(". ")}.
+<hr>
+<input type="date" bind:value={d2}>
+<br><br>
+{d2.split("-").join(". ")}.
+<hr>
+{sub} nap a különbség
+(kb. {(sub/365.2501).toFixed(2)} év)
