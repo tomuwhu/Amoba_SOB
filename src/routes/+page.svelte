ec5a32d2523a04694cdc23bf899f554158fe2a1e
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-11 09:45:53 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,22 @@
     var size = 12, win = false, next = "X"
     $: t = !win &&
     Array(size).fill().map(() => Array(size).fill().map(() => " "))
+    const valt = X => X == "X" ? "O" : "X"
 </script>
 <h1>Amőba</h1>
 <input type="range" bind:value={ size } min=5 max=15>
 {#if !win}
     <table>
+        <tr><th>Következik:</th></tr>
+        <tr><td class={ valt(next) }>{ valt(next) }</td></tr>
+    </table>
+    <hr>
+    <table>
     {#each t as row, y}
         <tr>
             {#each row as cell, x}
                 <td on:click={() => {               
-                    cell = next = next == "X" ? "O" : "X";
+                    cell = next = valt(next),
                     [[1, 0], [0, 1], [1, 1], [1, -1]].forEach(ir => {
                         var n = 0, xp = x, yp = y, [ix, iy] = ir
                         do n++, xp += ix, yp += iy 
