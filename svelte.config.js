49abb64c4869474b723b6ec6a371e176cbf1286c
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-05-13 20:08:17 +0200

    .

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	onwarn: (warning, handler) => {
-		if (warning.code === "a11y-click-events-have-key-events") return;
-		if (warning.code === "a11y-no-static-element-interactions") return;
+		if (warning.code.startsWith('a11y-')) return;
 		handler(warning);
   	},
 	kit: {
